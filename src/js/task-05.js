a6210a6a8d1dae51548f17853223558f130b0b4c
Yaroslav-ppp <83807204+Yaroslav-ppp@users.noreply.github.com>
2021-12-20 16:53:01 +0200

    hm?

--- src/js/task-05.js
+++
@@ -1,9 +0,0 @@
-const nameInput = document.querySelector("#name-input");
-const nameOutput = document.querySelector("#name-output");
-
-nameInput.addEventListener("input", (event) => {
-  const nameInput =
-    event.currentTarget.value.trim() === ""
-      ? (nameOutput.textContent = "Anonymous")
-      : (nameOutput.textContent = event.currentTarget.value);
-});
